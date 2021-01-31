6d74865e26eb3bc9b98a3658930d58298544f592
Allpacific <buhao1996@sina.com>
2021-01-31 15:06:55 +0800

    完成了登陆功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        // 取消缩进2格的规则
+        'indent': ['off', 2],
+        // 取消每个文件 保留最后一行是空行的规则
+        'eol-last': 0,
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
 }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入阿里字体图标
+import './assets/fonts/iconfont.css'
+// 导入axios
+import axios from 'axios'
+
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+
+// 挂载到vue原型上
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
